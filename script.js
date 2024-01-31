68d4a2f7d13f60709e0b5608d2170c2df9680834
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-31 11:16:44 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz-15uQjlwk1lR-VDZgwubVexiJC9Mewj9r6xAS_DniZO2IBQUaiHsaUklQtf7rGscnUA/exec'
-const form = document.forms
+const form = document.forms[]
