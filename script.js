ee071a4070c67fc64de49c042bf852b375dbbea5
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-31 11:19:15 +0530

    Update JS
    Update JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz-15uQjlwk1lR-VDZgwubVexiJC9Mewj9r6xAS_DniZO2IBQUaiHsaUklQtf7rGscnUA/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
+
