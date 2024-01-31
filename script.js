7700ff1be8ed5e7a8e95b0ce2b4ea5482edec586
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-01-31 11:15:29 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbz-15uQjlwk1lR-VDZgwubVexiJC9Mewj9r6xAS_DniZO2IBQUaiHsaUklQtf7rGscnUA/exec"
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz-15uQjlwk1lR-VDZgwubVexiJC9Mewj9r6xAS_DniZO2IBQUaiHsaUklQtf7rGscnUA/exec'
