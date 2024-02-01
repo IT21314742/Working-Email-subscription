0d8ba1e3a9f106a2ec40835838513a8d46f1f857
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-02-01 10:25:22 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { response } = require("express")
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz-15uQjlwk1lR-VDZgwubVexiJC9Mewj9r6xAS_DniZO2IBQUaiHsaUklQtf7rGscnUA/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
@@ -5,5 +7,5 @@ const msg = document.getElementById("msg")
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then()
+    .then(response)
 })
