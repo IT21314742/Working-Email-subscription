009995bb78cd7b8c21b6b50c42d3079bc3b2de49
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-02-01 10:38:21 +0530

    FD
    Format the Document

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Thank you for Subscribing!!"
-        setTimeout(function (){
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Thank you for Subscribing!!"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
 })
