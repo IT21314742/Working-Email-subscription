28373bb161e2850ad15116367c40671a49522b83
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-02-01 10:29:38 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', e => {
     .then(response => {
         msg.innerHTML = "Thank you for Subscribing!!"
         setTimeout(function() => {
-            msg
+            msg.innerHTML
         }, timeout);
     })
 })
