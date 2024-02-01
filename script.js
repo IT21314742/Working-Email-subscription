f87803c0ab3c429e2545ab3c4981ced1e130c5d8
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2024-02-01 10:24:42 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const msg = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData})
+    fetch(scriptURL, { method: 'POST', body: new FormData()})
 })
